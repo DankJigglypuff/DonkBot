dca0684c419e5bb6db93a30b0981c19525ba454e
DankJigglypuff <caleb.enderanimseries@gmail.com>
2016-07-15 19:44:27 -0500

    Add files via upload

---
+++ DonkBot.js
@@ -0,0 +1,108 @@
+var DiscordClient = require('discord.io');
+var currTime = "";
+
+var bot = new DiscordClient({
+    autorun: true,
+	email: "discordbottesting1@gmail.com",
+	password: "Redstoner11" 
+});
+
+bot.on('ready', function() {
+    console.log(bot.username + " - (" + bot.id + ")");
+});
+
+bot.on('message', function(user, userID, channelID, message, rawEvent) {
+	
+   if (message.indexOf("meme") > -1) {
+		
+        sendFiles(channelID, ["Clannad.jpg"]);
+		
+   }
+   if (message === "currenttime") {
+	   
+		bot.sendMessage({
+			to: channelID,
+			message: currTime +  " is the current time!"
+    });
+	   
+   }
+   if (message.indexOf("tits") > -1) {
+	   
+	   sendFiles(channelID, ["Plot.gif"]);
+	   
+	   }
+   
+   
+   if (message === "channelID") {
+	   
+	   bot.sendMessage({
+		   to: channelID,
+		   message: channelID + " is the channel ID"
+		   
+	});
+	   
+   }
+   });
+   
+
+
+bot.on('any', function() {
+	var currentTime = new Date().toLocaleTimeString('en-GB', { hour: "numeric", 
+                                             minute: "numeric" });
+	currTime = currentTime;
+	console.log(currentTime + " is the current time");
+	if (currentTime === "4:20 AM" || currentTime === "4:20 PM") {
+	   
+		sendFiles("160867765815607296", ["Clannad.jpg"]);
+		
+	}
+	
+});
+
+function sendFiles(channelID, fileArr, interval) {
+	var resArr = [], len = fileArr.length;
+	var callback = typeof(arguments[2]) === 'function' ? arguments[2] : arguments[3];
+	if (typeof(interval) !== 'number') interval = 1000;
+
+	function _sendFiles() {
+		setTimeout(function() {
+			if (fileArr[0]) {
+				bot.uploadFile({
+					to: channelID,
+					file: fileArr.shift()
+				}, function(err, res) {
+					if (err) {
+						resArr.push(err);
+					} else {
+						resArr.push(res);
+					}
+					if (resArr.length === len) if (typeof(callback) === 'function') callback(resArr);
+				});
+				_sendFiles();
+			}
+		}, interval);
+	}
+	_sendFiles();
+}
+
+
+/* Channel IDs
+MTG- 167080257831174144
+Shitposting- 160866222194425856
+Scheduling- 162620746022125569
+Meta- 183410005817556992
+Backroom- 161953525713207296
+Animu- 203056139317936129
+Admin- 160867765815607296
+*/
+
+/* Sending Messages
+    bot.sendMessage({
+        to: channelID,
+        message: "Desu Desu!"
+    });
+*/
+
+/* Sending Pictures
+	sendFiles(channelID, ["Clannad.jpg"]);
+*/
